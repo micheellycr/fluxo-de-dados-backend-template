2849cde781568af94ae883205a73d54b70a1fe7c
unknown <micheellycr@gmail.com>
2023-03-20 21:32:24 -0300

    resolução exercicios barbosa michelly cristine

--- src/index.ts
+++ src/index.ts
@@ -21,34 +21,105 @@ app.get("/accounts", (req: Request, res: Response) => {
 })
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
 
-    const result = accounts.find((account) => account.id === id) 
+    try {
+        const id = req.params.id
 
-    res.status(200).send(result)
+        const result = accounts.find((account) => account.id === id)
+
+        if(!result){
+            res.status(404)
+            throw new Error("Conta não encontrada verifique o id")
+        }
+        res.status(200).send(result)
+
+    } catch (error: any) {
+        console.log(error)
+        if(res.statusCode === 200){
+            res.status(500)
+        }
+    }
+
 })
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+    try {
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
+        const id = req.params.id
+
+        const accountIndex = accounts.findIndex((account) => account.id === id)
+
+        if(id[0] !== "a"){
+            res.status(400)
+            throw new Error("Id inválido, deve iniciar com a letra a");
+        }
+    
+        if (accountIndex >= 0) {
+            accounts.splice(accountIndex, 1)
+        }
+    
+        res.status(200).send("Item deletado com sucesso")
+        
+    } catch (error: any) {
+        console.log(error)
+        if(res.statusCode === 200){
+            res.status(500).send(error.message)
+        }
     }
 
-    res.status(200).send("Item deletado com sucesso")
+
+   
 })
 
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
     const newId = req.body.id as string | undefined
     const newOwnerName = req.body.ownerName as string | undefined
     const newBalance = req.body.balance as number | undefined
     const newType = req.body.type as ACCOUNT_TYPE | undefined
 
-    const account = accounts.find((account) => account.id === id) 
+    const account = accounts.find((account) => account.id === id)
+
+    if( newId !== undefined){
+        if(id[0] !== "a"){
+            res.status(400)
+            throw new Error("Id inválido, deve iniciar com a letra a");
+        }
+
+    }
+
+    if(newOwnerName !== undefined){
+        if(newOwnerName.length < 2){
+            res.status(400)
+            throw new Error("O nome deve ter dois ou mais caracteres");
+            
+        }
+    }
+
+    if(newBalance !== undefined){
+        if(typeof newBalance !== "number" ){
+            res.status(400)
+            throw new Error("Balance precisa ser um número");   
+        }
+
+        if(newBalance < 0){
+            res.status(400)
+            throw new Error("O valor do balance precisa ser maior ou igual a zero");   
+        }
+
+        if(newType !== undefined){
+            if(newType !== ACCOUNT_TYPE.GOLD &&
+                newType !== ACCOUNT_TYPE.PLATINUM &&
+                newType !== ACCOUNT_TYPE.BLACK){
+                   res.status(400)
+                   throw new Error("O valor passado deve ser um desses: Ouro, Platina ou Black");
+                }
+        }
+
+    }
 
     if (account) {
         account.id = newId || account.id
@@ -59,4 +130,10 @@ app.put("/accounts/:id", (req: Request, res: Response) => {
     }
 
     res.status(200).send("Atualização realizada com sucesso")
+    } catch (error: any) {
+        console.log(error)
+        if(res.statusCode === 200){
+            res.status(500).send(error.message)
+        }
+    }
 })
